1bff501309fd1d8491f7c4fc5a51f0d56a78c415
Sarah <sarahparr92@gmail.com>
2019-08-29 19:20:57 -0700

    Add Files for Student Daily Report Program
    Add Files for Student Daily Report Program

---
+++ TechAcademyDailyReport/TechAcademyDailyReport/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TechAcademyDailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("------------------------------");
+
+            Console.WriteLine("What course are you on?");
+            string whichCourse = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            string pageNumber = Console.ReadLine();
+            int pgNum = Convert.ToInt32(pageNumber);
+
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            string helpStatus = Console.ReadLine();
+            bool needingHelp = Convert.ToBoolean(helpStatus);
+
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string posExp = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            string hoursStudied = Console.ReadLine();
+            int hrsStudy = Convert.ToInt32(hoursStudied);
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}

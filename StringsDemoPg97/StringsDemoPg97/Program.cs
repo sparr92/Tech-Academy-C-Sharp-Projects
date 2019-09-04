c08a7f065eb96f15a136d433bfde3f09b9addc03
Sarah <sarahparr92@gmail.com>
2019-09-04 15:59:17 -0700

    Add Strings Demo App from pg. 97 of C# Course
    Add Strings Demo App from pg. 97 of C# Course

---
+++ StringsDemoPg97/StringsDemoPg97/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsDemoPg97
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Marge";
+            string lastName = "Simpson";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Chapter One: Starts and Beginnings \n \t \"Swim, swim, swim,\" thought the whale, flapping his flappers.");
+
+            lastName = lastName.ToUpper();
+
+            Console.WriteLine(firstName + " " + lastName);
+            Console.WriteLine(" ");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}

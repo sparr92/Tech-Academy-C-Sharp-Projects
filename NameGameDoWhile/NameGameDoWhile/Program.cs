fde2246386121485ec861e3910606535f9eba84c
Sarah <sarahparr92@gmail.com>
2019-09-04 14:35:44 -0700

    Add Program Files for While and Do While Statement Apps
    Add Program Files for While and Do While Statement Apps, pg. 94 of C# course

---
+++ NameGameDoWhile/NameGameDoWhile/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileAndDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is my name?");
+            string name = Convert.ToString(Console.ReadLine());
+            bool isGuessed = name == "Sarah";
+
+            do
+            {
+                switch (name)
+                {
+                    case "Emily":
+                        Console.WriteLine("Nope, sorry! Try again.");
+                        name = Convert.ToString(Console.ReadLine());
+                        break;
+
+                    case "Sophie":
+                        Console.WriteLine("Nope, sorry! Try again.");
+                        name = Convert.ToString(Console.ReadLine());
+                        break;
+
+                    case "Katie":
+                        Console.WriteLine("Nope, sorry! Try again.");
+                        name = Convert.ToString(Console.ReadLine());
+                        break;
+
+                    case "Sarah":
+                        Console.WriteLine("You got it! Good job, friend-o.");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("Nope, sorry! Try again.");
+                        name = Convert.ToString(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed);
+
+            Console.Read();
+        }
+    }
+}
---
+++ WhileLoopApp/WhileLoopApp/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm thinking of a number between 1 and 10. Make a guess!");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = false;
+
+            while (!isGuessed)
+            {
+                switch (number)
+                {
+                    case 1:
+                        Console.WriteLine("You guessed 1. That's not it! Try again.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("You guessed 2. That's not it! Try again.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine("You guessed 3. That's not it! Try again.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 4:
+                        Console.WriteLine("You guessed 4. That's not it! Try again.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 5:
+                        Console.WriteLine("You guessed 5. That's not it! Try again.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 6:
+                        Console.WriteLine("You guessed 6. That's not it! Try again.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 7:
+                        Console.WriteLine("You guessed 7. That's right!");
+                        isGuessed = true;
+                        break;
+                    case 8:
+                        Console.WriteLine("You guessed 8. That's not it! Try again.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 9:
+                        Console.WriteLine("You guessed 9. That's not it! Try again.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 10:
+                        Console.WriteLine("You guessed 10. That's not it! Try again.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("I said 1 to 10! Try again, dum dum.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}

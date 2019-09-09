bb55759516357b2051b8917acbf943d08f2a263d
Sarah <sarahparr92@gmail.com>
2019-09-09 14:00:31 -0700

    Add Program Files for Try/Catch Exercise, pg. 106
    Add Program Files for Try/Catch Exercise, pg. 106

---
+++ IntAppPg106/IntAppPg106/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int>() { 5, 3, 10, 2, 6, 4 };
+        Console.WriteLine("Please enter a number to divide by each number in my list: ");
+        try
+        {
+            foreach (int number in numbers)
+            {
+                int userNum = Convert.ToInt32(Console.ReadLine());
+                int dividend = number / userNum;
+                Console.WriteLine(number + " divided by " + userNum + " equals " + dividend);
+            }
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please do not enter zero.");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please enter a whole number.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("You have emerged from the try/catch block");
+            Console.ReadLine();
+        }
+    }   
+}

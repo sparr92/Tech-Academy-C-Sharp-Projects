d6a2820c5e10e678d008951700f77aa237b74118
Sarah <sarahparr92@gmail.com>
2019-09-09 18:21:45 -0700

    Add Files for Calculator App, pg. 111
    Add Files for Calculator App, pg. 111

---
+++ ClassMethodsMathApp111/ClassMethodsMathApp111/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodsMathApp111
+{
+    public class Calculator
+    {
+        // Add integer to 37
+        public int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        // Multiply two numbers
+        public int Multiplication(int num1, int num3)
+        {
+            return num1 * num3;
+        }
+        // Subtracts smaller number from bigger number
+        public int MultDiv(int num1, int num4, int num5)
+        {
+            return (num1 * num4) / num5;
+        }
+
+    }
+}
---
+++ ClassMethodsMathApp111/ClassMethodsMathApp111/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodsMathApp111
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num1;
+            int num2 = 37;
+            int num3 = 15;
+            int num4 = 10;
+            int num5 = 5;
+
+            Calculator calculator = new Calculator();
+            Console.WriteLine("Please enter an integer you would like to perform calculations on: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1 + " + " + num2 + " = " + calculator.Addition(num1, num2));
+            Console.WriteLine(num1 + " * " + num3 + " = " + calculator.Multiplication(num1, num3));
+            Console.WriteLine("(" + num1 + " * " + num4 + ") / " + num5 + " = " + calculator.MultDiv(num1, num4, num5));
+
+            Console.ReadLine();
+        }
+    }
+}

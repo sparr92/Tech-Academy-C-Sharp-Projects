d478d937e1e4de1f588cb2336340f3307a853d10
Sarah <sarahparr92@gmail.com>
2019-09-10 13:21:20 -0700

    Add Project Files for Drill on Pg. 113
    Add Project Files for Drill on Pg. 113

---
+++ ConsoleAppPg113/ConsoleAppPg113/Calc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPg113
+{
+    class Calc
+    {
+        public void Addition (int num1, int num2)
+        {
+            int sum = num1 + 14;
+            Console.WriteLine(num1 + " + " + 14 + " = " + sum);
+            Console.WriteLine(num2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleAppPg113/ConsoleAppPg113/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPg113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc calc = new Calc();
+            Console.WriteLine("Please enter a number you would like to add 14 to: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a number you would like to display: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            calc.Addition(number1, number2);
+            calc.Addition(num1: 5, num2: 8);
+            Console.ReadLine();
+        }
+    }
+}

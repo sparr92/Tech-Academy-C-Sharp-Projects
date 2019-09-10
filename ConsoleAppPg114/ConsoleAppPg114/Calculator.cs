0722bc131619ae60c61fecefc228284fa417beb2
Sarah <sarahparr92@gmail.com>
2019-09-10 14:51:01 -0700

    Add Files for Drill on Pg. 114
    Add Files for Drill on Pg. 114

---
+++ ConsoleAppPg114/ConsoleAppPg114/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPg114
+{
+    public class Calculator
+    {
+        public int Math(int num1)
+        {
+            return num1 + 17;
+        }
+        public int Math(double num2)
+        {
+            return Convert.ToInt32(num2 * 3);
+        }
+        public int Math(string num3)
+        {
+            return Convert.ToInt32(num3)/4;
+        }
+
+    }
+}
---
+++ ConsoleAppPg114/ConsoleAppPg114/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPg114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine(calculator.Math(num1: 12));
+            Console.WriteLine(calculator.Math(num2: 5.67));
+            Console.WriteLine(calculator.Math(num3: "40"));
+            Console.ReadLine();
+        }
+    }
+}

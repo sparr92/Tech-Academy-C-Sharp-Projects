a2890360901b42dc6882013fd06a6578b6af55e5
Sarah <sarahparr92@gmail.com>
2019-09-10 15:34:17 -0700

    Add Files for Drill on Pg. 115
    Add Files for Drill on Pg. 115

---
+++ ConsoleAppPg115/ConsoleAppPg115/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPg115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calc calc = new calc();
+            Console.WriteLine("Please enter a number you would like to add 15 to: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter second number you would like to add to the total (optional): ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(calc.Total(number1, number2));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleAppPg115/ConsoleAppPg115/calc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPg115
+{
+    public class calc
+    {
+        public int Total(int num1, int num2 = 0)
+        {
+            return num1 + num2 + 15;
+        }
+    }
+}

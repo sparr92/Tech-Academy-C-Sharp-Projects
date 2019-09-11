9d9217675b9d0f26dce3dae912aaa65fa402f3b8
Sarah <sarahparr92@gmail.com>
2019-09-10 22:10:11 -0700

    Add Project Files for Drill on Pg. 124
    Add Project Files for Drill on Pg. 124

---
+++ ConsoleAppPg124/ConsoleAppPg124/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPg124
+{
+    public abstract class Person
+    {
+        public List<string> Persons { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public abstract void SayName();
+    }
+}
+

a4fe00be46d08e801affce203e0b0deb76f895e7
Sarah <sarahparr92@gmail.com>
2019-09-10 19:57:51 -0700

    Add Files for Drill on Pg. 120
    Add Files for Drill on Pg. 120

---
+++ ConsoleAppPg120/ConsoleAppPg120/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPg120
+{
+    public class Person
+    {
+        public List<string> Persons { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}

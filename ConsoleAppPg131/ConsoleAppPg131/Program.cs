999f57afc31447c37c5bb4634f09716c11323f90
Sarah <sarahparr92@gmail.com>
2019-09-12 13:13:22 -0700

    Add Files for Drill on Pg 131
    Add Files for Drill on Pg 131

---
+++ ConsoleAppPg131/ConsoleAppPg131/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPg131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var officeFurniturePcs = new List<string>() { "desk", "chair", "couch", "bookcase", "table"};
+
+            var perRoomNumComputers = new List<int>() { 18, 2, 1, 4, 1, 1, 3, 1, 2};
+            perRoomNumComputers = perRoomNumComputers.OrderBy(n => n).ToList();
+
+            foreach (string officeFurniturePc in officeFurniturePcs)
+            {
+                Console.WriteLine(officeFurniturePc);
+            }
+            foreach (int perRoomNumComputer in perRoomNumComputers)
+            {
+                Console.WriteLine(perRoomNumComputer);
+            }
+            Console.ReadLine();
+        }
+    }
+}

9cddcc6f94280b983457ad6b83e2ef0c76b8a8ae
Sarah <sarahparr92@gmail.com>
2019-09-12 00:29:17 -0700

    Add Files for Drill on Pg. 129
    Add Files for Drill on Pg. 129

---
+++ ConsoleAppPg129/ConsoleAppPg129/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPg129
+{
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+    public static bool operator ==(Employee employee, Employee employee2)
+        {
+            if (employee.Id == employee2.Id)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator !=(Employee employee, Employee employee2)
+        {
+            return employee.Id != employee2.Id;
+        }
+        public override bool Equals(Object obj)
+        {
+            var emp = obj as Employee;
+
+            if (emp == null)
+            {
+                return false;
+            }
+            return this.Id.Equals(emp.Id);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ ConsoleAppPg129/ConsoleAppPg129/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPg129
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee example = new Employee();
+
+            Employee employee = new Employee() { FirstName = "John", LastName = "Doe", Id = 001 };
+            Employee employee2 = new Employee() { FirstName = "Jane", LastName = "Doe", Id = 002 };
+
+            Console.WriteLine("Do they match? True or false: ");
+            Console.WriteLine(employee == employee2);
+            Console.ReadLine();
+        }
+    }
+}

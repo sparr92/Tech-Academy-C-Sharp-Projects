e5f8974581fa410870d4690d4549f2df71e0a57c
Sarah <sarahparr92@gmail.com>
2019-09-12 15:08:00 -0700

    Add Files for Drill on Pg 137
    Add Files for Drill on Pg 137

---
+++ ConsoleAppPg137/ConsoleAppPg137/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+struct Numbers {
+    decimal Amount;
+    public void GetAmount(decimal amt) {
+        Amount = amt;
+    }
+    public void Print()
+    {
+        Console.WriteLine(Amount);
+    }
+};
+public class Amount
+{
+    public static void Main(string[] args)
+    {
+        Numbers num1 = new Numbers();
+        Numbers num2 = new Numbers();
+
+        num1.GetAmount(3.38m);
+        num2.GetAmount(5.689m);
+
+        num1.Print();
+        num2.Print();
+
+        Console.ReadKey();
+    }
+}

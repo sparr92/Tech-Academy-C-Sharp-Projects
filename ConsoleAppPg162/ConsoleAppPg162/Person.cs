4e86e8dfcf9cfd87da6aa27d83d0a815ef6de0a5
Sarah <sarahparr92@gmail.com>
2019-09-30 12:49:25 -0700

    Add Project Files for Drill on Pg 162
    Add Project Files for Drill on Pg 162

---
+++ ConsoleAppPg162/ConsoleAppPg162/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPg162
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int IdNumber { get; set; }
+
+        public Person(string fName, string lName)
+            : this(fName, lName, 0)
+        {
+        }
+        public Person(string fName, string lName, int id)
+        {
+            FirstName = fName;
+            LastName = lName;
+            IdNumber = id;
+        }
+
+    }
+}
---
+++ ConsoleAppPg162/ConsoleAppPg162/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPg162
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.Create a const variable
+            // 2.Create a variable using the keyword "var".
+            // 3.Chain two constructors together.
+
+            var Students = new List<Person>()
+            {
+                new Person("Sarah", "Parr"),
+                new Person("John", "Parr", 4628)
+            };
+
+            const string contactMsg = "Please contact the admin for a new ID number.";
+            foreach (var student in Students)
+            {
+                Console.WriteLine($"Welcome, {student.FirstName} {student.LastName}! Your ID number is {student.IdNumber}.");
+                if (student.IdNumber == 0)
+                    Console.WriteLine(contactMsg);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}

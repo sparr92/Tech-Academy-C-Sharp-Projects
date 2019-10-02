e314e2788e3483db815c91fd20af7874edecba29
Sarah <sarahparr92@gmail.com>
2019-10-02 13:49:31 -0700

    Add Project Files for Drill on Pg. 165
    Add Project Files for Drill on Pg. 165 (Try/Catch + Exception Handling)

---
+++ ConsoleAppPg165/ConsoleAppPg165/InvalidAgeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPg165
+{
+    public class InvalidAgeException : Exception
+    {
+        public InvalidAgeException()
+            : base() { }
+        public InvalidAgeException(string message)
+            : base(message) { }
+    }
+}
---
+++ ConsoleAppPg165/ConsoleAppPg165/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPg165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                var year = DateTime.Now.Year;
+                var birthYear = year - age;
+                if (age == 0)
+                {
+                    throw new InvalidAgeException("Please don't enter 0: ");
+                }
+                else if (age < 0)
+                {
+                    throw new InvalidAgeException("Please enter a positive whole integer: ");
+                }
+                else
+                {
+                    Console.WriteLine("You were born in " + birthYear);
+                }
+            }
+            catch (InvalidAgeException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occurred. Please enter a valid age: ");
+            }
+            Console.ReadLine();
+        }
+    }
+}

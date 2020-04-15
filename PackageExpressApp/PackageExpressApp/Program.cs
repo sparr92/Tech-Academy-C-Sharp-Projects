dde69f8f8983b1cf5b5790a371f743ee0f696269
Sarah <sarahparr92@gmail.com>
2020-04-14 20:29:56 -0500

    Edited Code for Re-Submit
    Edited Code for Re-Submit

--- PackageExpressApp/PackageExpressApp/Program.cs
+++ PackageExpressApp/PackageExpressApp/Program.cs
@@ -22,27 +22,35 @@ namespace PackageExpressApp
                 Console.ReadLine();
             }
 
-            else
-            Console.WriteLine("Please enter package width: ");
-            int pkgWidth = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Please enter package height: ");
-            int pkgHeight = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Please enter package length: ");
-            int pkgLength = Convert.ToInt32(Console.ReadLine());
-
-            if (pkgWidth + pkgHeight + pkgLength > 50)
+            else if (pkgWt <= 50)
             {
-                Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
-                Console.ReadLine();
+                Console.WriteLine("Please enter package width: ");
+                decimal pkgWidth = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Please enter package height: ");
+                decimal pkgHeight = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Please enter package length: ");
+                decimal pkgLength = Convert.ToDecimal(Console.ReadLine());
+
+
+                decimal totDim = (pkgWidth + pkgHeight + pkgLength);
+                decimal quote = (totDim * pkgWt) / 100;
+                if (totDim > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                    Console.ReadLine();
+                }
+
+                else if (totDim < 50)
+                {
+                    Console.WriteLine("Your estimate is $" + quote);
+                    Console.ReadLine();
+                }
+                    
             }
 
-            else
 
-
-                Console.WriteLine("Your estimate is $" + (pkgWidth + pkgHeight + pkgLength));
-                Console.ReadLine();
         }
     }
 }
